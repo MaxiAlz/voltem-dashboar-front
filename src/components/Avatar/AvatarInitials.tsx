4ce2560c71f9d76dacb2814804a99d5ea2a5fdd9
Maxi Alzugaray <maxialz99@hotmail.com>
2024-05-31 21:55:02 -0300

    modifico ui de componente avatar

--- src/components/Avatar/AvatarInitials.tsx
+++ src/components/Avatar/AvatarInitials.tsx
@@ -6,9 +6,9 @@ interface AvatarInitialsProps {
 
 const AvatarInitials = ({ name, lastName }: AvatarInitialsProps) => {
   return (
-    <div className="inline-flex items-center justify-center w-12 h-12 text-xl text-white bg-primary rounded-full">
-      {name.slice(0, 1)}
-      {lastName.slice(0, 1)}
+    <div className="inline-flex items-center justify-center w-12 h-12 text-xl text-white font-medium bg-primary rounded-full">
+      {name.slice(0, 1).toUpperCase()}
+      {lastName.slice(0, 1).toUpperCase()}
     </div>
   );
 };
